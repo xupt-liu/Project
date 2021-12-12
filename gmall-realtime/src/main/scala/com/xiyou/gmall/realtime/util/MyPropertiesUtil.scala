d3f097a11268a77e3206a8a3e6d4b9d19487b292
mono <15103574903@163.com>
2021-12-12 13:33:17 +0800

    SparkStreaming实时处理的相关配置文件，包括redis,kafka以及读取配置文件的工具类

---
+++ gmall-realtime/src/main/scala/com/xiyou/gmall/realtime/util/MyPropertiesUtil.scala
@@ -0,0 +1,26 @@
+package com.xiyou.gmall.realtime.util
+
+import java.io.InputStreamReader
+import java.nio.charset.StandardCharsets
+import java.util.Properties
+
+/**
+ * Author: xy_mono
+ * Date: 2020/12/10
+ * Desc: 读取配置文件的工具类
+ */
+object MyPropertiesUtil {
+  def main(args: Array[String]): Unit = {
+    val prop: Properties = MyPropertiesUtil.load("config.properties")
+    println(prop.getProperty("kafka.broker.list"))
+  }
+
+  def load(propertiesName: String): Properties = {
+    val prop: Properties = new Properties()
+    //加载指定的配置文件
+    prop.load(new InputStreamReader(
+      Thread.currentThread().getContextClassLoader.getResourceAsStream(propertiesName),
+      StandardCharsets.UTF_8))
+    prop
+  }
+}
