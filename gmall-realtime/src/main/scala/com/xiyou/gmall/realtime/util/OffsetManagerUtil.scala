51918619af9b278b294a7387eae3d660d3385c13
mono <15103574903@163.com>
2022-01-09 19:47:01 +0800

    工具类，手动提交偏移量，将偏移量保存到redis中

---
+++ gmall-realtime/src/main/scala/com/xiyou/gmall/realtime/util/OffsetManagerUtil.scala
@@ -0,0 +1,23 @@
+package com.xiyou.gmall.realtime.util
+
+/**
+ * @author: Administrator
+ * @date: 2022/1/8
+ * @description:维护偏移量的工具类
+ */
+
+
+object OffsetManagerUtil {
+  //实现功能一：获取offset
+  def getOffset(): Unit = {
+
+  }
+
+
+  //实现功能二：保存offset
+  def saveOffset(): Unit = {
+
+  }
+
+
+}
